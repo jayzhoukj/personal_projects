3d5db5aea2d0a19f2271f1d4a12946b5c2559581
jayzhoukj <jayzhoukj@gmail.com>
2020-12-05 23:41:10 +0800

    merge mp3 normalization (pyautogui)

--- MP3 Downloader/main.py
+++ MP3 Downloader/main.py
@@ -1,66 +1,139 @@
 from __future__ import unicode_literals
 
-import shutil
-import os
 import eyed3
 import ffmpeg
+import os
+import pyautogui as pag
+import shutil
+import time
 import youtube_dl
 
-def run():
-    # Setting download parameters
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '320',
-        }],
-    }
+class mp3downloader:
 
-    while True:
-        main_cmd = input('Continue? [y/n] ')
-        if main_cmd.lower() == 'n':
-            print('Exiting program...')
-            return
-        elif main_cmd.lower() == 'y':
-            pass
-        else:
-            print('CommandError: Invalid Input Command!')
-            return
+    def __init__(self):
 
-        # Collecting download url and file name from user
-        url_input = input('YouTube URL: ')
-        file_name = input('File Name [<artist> - <title>]: ') + '.mp3'
+        self.original = []
+        self.tempName = []
+        self.root_dir = os.getcwd()
+        self.dest_dir = r'C:\Users\Kai Jing\Music\Songs\normalized\00 normalize'
 
-        artist = file_name.split(' - ')[0]
-        title = file_name.split(' - ')[1][:-4]
+        # Download parameters
+        self.ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '320',
+            }],
+        }
 
-        # Download mp3 file
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url_input])
+    def download(self):
 
-        # Rename downloaded mp3 file
-        downloaded_name = [name for name in os.listdir() if name.endswith('.mp3')][0]
+        while True:
+            main_cmd = input('Continue? [y/n] ')
+            if main_cmd.lower() == 'n':
+                print('Finished downloading...')
+                return
+            elif main_cmd.lower() == 'y':
+                pass
+            else:
+                print('CommandError: Invalid Input Command!')
+                return
 
-        src = os.path.join(os.getcwd(), downloaded_name)
-        dest = os.path.join(os.getcwd(), file_name)
+            # Collecting download url and file name from user
+            url_input = input('YouTube URL: ')
+            file_name = input('File Name [<artist> - <title>]: ') + '.mp3'
 
-        if file_name in os.listdir():
-            os.remove(dest)
+            artist = file_name.split(' - ')[0]
+            title = file_name.split(' - ')[1][:-4]
+
+            # Download mp3 file
+            with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
+                ydl.download([url_input])
+
+            # Rename downloaded mp3 file
+            youtube_uri = url_input.split('=')[-1]
+            downloaded_name = [name for name in os.listdir() if youtube_uri in name][0]
+
+            src = os.path.join(self.root_dir, downloaded_name)
+            dest = os.path.join(self.root_dir, file_name)
+
+            if file_name in os.listdir():
+                os.remove(dest)
+                os.rename(src, dest)
+            else:
+                os.rename(src, dest)
+
+            # Update mp3 ID3 tags
+            audio = eyed3.load(file_name)
+
+            audio.tag.artist = artist
+            audio.tag.title = title
+
+            audio.tag.save()
+
+    def normalize(self):
+
+        self.original = [file for file in os.listdir() if file.endswith('.mp3')]
+        self.tempName = [str(i)+'.mp3' for i in range(len(self.original))]
+
+        # Map from chinese to english
+        for i, file in enumerate(self.original):
+            src = os.path.join(self.root_dir, file)
+            dest = os.path.join(self.root_dir, self.tempName[i])
+
             os.rename(src, dest)
-        else:
+
+        for name in self.tempName:
+            pag.click(20, 1060)
+            time.sleep(1)
+
+            pag.typewrite([char for char in 'mp3gain'] + ['enter'], interval=0.1)
+            time.sleep(1)
+
+            # Add file
+            pag.click(40, 80)
+            time.sleep(1)
+
+            # Select file
+            pag.click(500, 95)
+            pag.typewrite([char for char in self.root_dir] + ['enter'], interval=0.1)
+            time.sleep(.5)
+
+            pag.click(330, 530)
+            pag.typewrite([char for char in name] + ['enter'], interval=0.1)
+            time.sleep(.5)
+
+            # Analyze track
+            pag.click(210, 80)
+            time.sleep(7)
+
+            # Track gain
+            pag.click(310, 80)
+            time.sleep(5)
+
+            # Exit program
+            pag.click(1895, 10)
+
+        # Reverse map from english to chinese
+        for i, file in enumerate(self.tempName):
+            src = os.path.join(self.root_dir, file)
+            dest = os.path.join(self.root_dir, self.original[i])
+
             os.rename(src, dest)
 
-        # Update mp3 ID3 tags
-        audio = eyed3.load(file_name)
+    def moveToDest(self):
 
-        audio.tag.artist = artist
-        audio.tag.title = title
+        for file in self.original:
+            src = os.path.join(self.root_dir, file)
+            dest = os.path.join(self.dest_dir, file)
 
-        audio.tag.save()
-
-        # Move downloaded mp3 file to destination folder
-        shutil.move(dest, os.path.join(r'C:\Users\Kai Jing\Music\Songs\normalized\00 normalize', file_name))
+            shutil.move(src, dest)
 
 if __name__ == '__main__':
-    run()
+
+    musicDownloader = mp3downloader()
+
+    musicDownloader.download()
+    musicDownloader.normalize()
+    musicDownloader.moveToDest()
